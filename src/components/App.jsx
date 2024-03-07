85ef0609b4b8fe7199fc28c399d861aac16b74f0
Sammy Gonzalez III <sammygonzo951@gmail.com>
2024-03-06 22:36:25 -0800

    Added simple place holder elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+
+
 function App(){
     return (
         <div>
-            <p>Hello World!</p>
+            <h1>Welcome to Codeblitz!</h1>
+            <p>Practice your typing skills like a true developer. Click **start** to begin!</p>
+
+            <br />
+
+            <p id="snippet"></p>
+            <p id="message"></p>
+            <p id="time"></p>
+
+            <br />
+
+            <div>
+                <input type="hidden" aria-label="current word" id="typed=value" />
+                <button type="button" id="start">Start</button>
+            </div>
         </div>
     );
 }
