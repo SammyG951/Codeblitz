faacd55d287a483e17b7f9d460f87a97dbf6eb1c
Sammy Gonzalez III <sammygonzo951@gmail.com>
2024-03-27 13:22:31 -0700

    Changed . to !

--- src/components/partials/Welcome.jsx
+++ src/components/partials/Welcome.jsx
@@ -4,7 +4,7 @@ function Welcome() {
     return(
         <div id="welcome">
             <img src="images/CodeBlitz!_Logo.svg" alt="CodeBlitz!"/>
-            <h3>Practice your typing skills like a true developer. Click **start** to begin!</h3>
+            <h3>Practice your typing skills like a true developer! Click **start** to begin!</h3>
         </div>
     );
 }
