e080d8ba857221ad05fda9b0c5d57c393c8d80bc
Sammy Gonzalez III <sammygonzo951@gmail.com>
2024-03-27 11:39:52 -0700

    Added styles to Welcome partial

--- src/components/partials/Welcome.jsx
+++ src/components/partials/Welcome.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 function Welcome() {
     return(
-        <div>
-            <img src="images/CodeBlitz!_Logo.png" alt="CodeBlitz!"/>
-            <h1>Welcome to Codeblitz!</h1>
-            <p>Practice your typing skills like a true developer. Click **start** to begin!</p>
+        <div id="welcome">
+            <img src="images/CodeBlitz!_Logo.svg" alt="CodeBlitz!"/>
+            <h3>Practice your typing skills like a true developer. Click **start** to begin!</h3>
         </div>
     );
 }
