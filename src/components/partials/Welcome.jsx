f2b151019d39a06e4d536fff770caf04faf97641
Sammy Gonzalez III <sammygonzo951@gmail.com>
2024-03-27 10:51:34 -0700

    Connected styles.css and added CodeBlitz logo

--- src/components/partials/Welcome.jsx
+++ src/components/partials/Welcome.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function Welcome() {
     return(
         <div>
+            <img src="images/CodeBlitz!_Logo.png" alt="CodeBlitz!"/>
             <h1>Welcome to Codeblitz!</h1>
             <p>Practice your typing skills like a true developer. Click **start** to begin!</p>
         </div>
